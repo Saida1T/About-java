9afe8c94ccb4ab02f579939489f3f80ec9fe37be
Saida T <saidatursunova09@gmail.com>
2023-12-05 20:24:50 +0600

    Added project using java veriables

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        double height = 1.70;
+    String name = "Saida";
+    System.out.println("Name: " + "Saida");
+    System.out.println("Age: " + 20);
+    System.out.println("Height: " + 1.70);
+    }
+}
