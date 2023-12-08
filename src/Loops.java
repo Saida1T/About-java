32db0297421fc0cf4fe11eea8c2097caaab23d5d
Saida T <saidatursunova09@gmail.com>
2023-12-08 20:27:05 +0600

    Added a file loops.java

---
+++ src/Loops.java
@@ -0,0 +1,14 @@
+public class Loops {
+    public static void main(String[] args) {
+        // Цикл while
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Цикл while: " + i);
+            i++;
+    }
+        //Цикл for
+        for (int j = 0; j < 5; j++) {
+            System.out.println("Цикл for: " + j);
+        }
+        }
+}
