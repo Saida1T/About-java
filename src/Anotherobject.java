2b108ed79b60ee325acdb695199226c509c65840
Saida T <saidatursunova09@gmail.com>
2023-12-12 20:38:21 +0600

    Added files related to Classes and Objects

---
+++ src/Anotherobject.java
@@ -0,0 +1,9 @@
+public class Anotherobject {
+    public int addNumbers(int a, int b)
+    {
+        return a + b;
+    }
+    public String getGreeting() {
+        return "Привет, мир!";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Myobject myobject = new Myobject();
+        myobject.someMethod();
+    }
+}
