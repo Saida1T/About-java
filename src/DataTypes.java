9dc1d92debcc4fe5f1278ec9a50f158fe7e28608
Saida T <saidatursunova09@gmail.com>
2023-12-14 12:25:13 +0600

    Added DataTypes.java

---
+++ src/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int intValue = 42;
+        long longValue = 123456789L;
+        float floatValue = 3.14f;
+        double doubleValue = 2.71828;
+        char charValue = 'A';
+        boolean booleanValue = true;
+        System.out.println("int: " + intValue);
+        System.out.println("long: " + longValue);
+        System.out.println("float: " + floatValue);
+        System.out.println("double: " + doubleValue);
+        System.out.println("char: " + charValue);
+        System.out.println("boolean: " + booleanValue);
+    }
+}
+
